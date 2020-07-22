332ec986ee35da2a9b127c7b5627e3ee57b5996f
Sudhakar <Sudhakar12161@gmail.com>
2020-07-21 22:01:10 -0700

    Updated parameter values based on the new dag code

--- dag/email_sender.py
+++ dag/email_sender.py
@@ -1,19 +1,26 @@
 import smtplib
 from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 #this is reusable module for email sending
 #Need to pass the below parameters to send email
-def send_email(EMAIL_ADDRESS,EMAIL_PASSWORD,sub,to,bcc,content,file_name,data_set):
-
-    msg = EmailMessage()
+def send_email(EMAIL_ADDRESS, EMAIL_PASSWORD, sub, to, bcc, body, file_name):
+    
+    COMMASPACE = ', '
+    msg = MIMEMultipart()
     msg['Subject'] = sub
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = to
-    msg['Bcc'] = bcc
-    msg.set_content('{0}\n\n'.format(content))
-
-    msg.add_alternative(data_set,subtype='html')
-
+    msg['Bcc'] = COMMASPACE.join(bcc)
+    for key,value in body.items():
+        msg.attach(MIMEText(key,'plain'))
+        msg.attach(MIMEText(value,'html'))
+    #part1 = MIMEText(content,'plain')
+    #part2 = MIMEText(data_set,'html')
+    #msg.attach(part1)
+    #msg.attach(part2)
+   
     with smtplib.SMTP_SSL('smtp.gmail.com',465) as smp:
         smp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
         smp.ehlo()
